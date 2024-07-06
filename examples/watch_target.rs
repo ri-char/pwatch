4f40600a08f855afe56d5206fbb5ae8ef5617471
riChar <wxsychi@163.com>
2024-07-06 12:54:49 +0800

    add multi-thread test target

--- examples/watch_target.rs
+++ examples/watch_target.rs
@@ -1,9 +1,36 @@
+use nix::libc;
+use std::sync::atomic::Ordering::SeqCst;
+use std::{sync::atomic::AtomicU64, thread};
+
+const COUNTER: AtomicU64 = AtomicU64::new(0);
+
 fn main() {
     println!("pid: {}", std::process::id());
-    let mut counter = 0;
+    let builder = thread::Builder::new();
+    builder
+        .spawn(|| {
+            let tid = unsafe { libc::gettid() };
+            loop {
+                COUNTER.store(COUNTER.load(SeqCst) + 1, SeqCst);
+                println!(
+                    "[{}] ptr: {:p} {}",
+                    tid,
+                    COUNTER.as_ptr(),
+                    COUNTER.load(SeqCst)
+                );
+                std::thread::sleep(std::time::Duration::from_secs(1));
+            }
+        })
+        .unwrap();
+    let tid = unsafe { libc::gettid() };
     loop {
-        counter += 1;
-        println!("ptr: {:p} {}", &counter, counter);
+        COUNTER.store(COUNTER.load(SeqCst) + 1, SeqCst);
+        println!(
+            "[{}] ptr: {:p} {}",
+            tid,
+            COUNTER.as_ptr(),
+            COUNTER.load(SeqCst)
+        );
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
